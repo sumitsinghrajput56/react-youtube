447d8fb27e87acdb66e7af49755e7b9a37ea993d
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2026-01-26 02:26:11 +0530

    react-redux installed initial setup and made menu hide show functionality component devide

---
+++ src/components/Head.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
+
+const Head = () => {
+  const dispatch = useDispatch();
+
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
+  return (
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+      <div className="flex col-span-1">
+        <img
+          className="h-8 cursor-pointer"
+          alt="menu"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr-cwMeLLj8MfIo3HoXJKFcOFB1g9U4DDMHA&s"
+          onClick={() => toggleMenuHandler()}
+        />
+
+        <a href="/">
+          <img
+            className="h-8 mx-2"
+            alt="youtube-logo"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzGhpfyPfZxmMk39fDQJRymuKUvP7kXzpTMg&s"
+          />
+        </a>
+      </div>
+      <div className="col-span-10  px-10">
+        <input
+          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
+          type="text"
+        />
+        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+          🔍
+        </button>
+      </div>
+      <div className="col-span-1">
+        <img
+          className="h-8"
+          alt="user-icon"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV-Gh6uC11b9BUzfJ1OAuC3MgwwQdOLZL7PA&s"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
---
+++ src/utils/store.js
@@ -0,0 +1,12 @@
+import appSlice from "./appSlice"
+const { configureStore } = require("@reduxjs/toolkit");
+
+
+
+const store = configureStore({
+reducer: {
+    app: appSlice,
+}
+});
+
+export default store;
